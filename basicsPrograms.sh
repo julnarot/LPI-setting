178bce8aa947e38e79a9dc784f3ed88d429f99ee
@Julnarot <rauljhonatan.tola@gmail.com>
2022-03-07 11:34:54 -0500

    it was change vim setting file

--- basicsPrograms.sh
+++ basicsPrograms.sh
@@ -1,5 +1,5 @@
 cd ~/
-mkdir jota
+# mkdir jota
 apt-get update && apt-get upgrade -y
 apt-get dist-upgrade
 apt-get install build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev cmake 
@@ -8,9 +8,9 @@ apt-get install default-jre default-jdk
 apt-get install libpq-dev
 apt-get install python python-dev python-pip
 apt-get install python3.7 python3-dev libpq-dev libpython3.7-dev python3-pip
-apt-get install ssh git wget curl
+apt-get install ssh git tig wget curl 
 apt-get install zsh tmux
-apt-get install virtualbox
+
 apt-get install autoremove -y
 apt-get remove vim-tiny -y
 apt-get install vim 
@@ -18,14 +18,27 @@ pip install virtualenv
 pip install psycopg2-binary
 pip3 install psycopg2
 #### Multimedia ####
-apt-get install flashplugin-installer vlc
-#### NetWorking ####
-apt-get install nmap
+apt-get install flashplugin-installer 
+
+#### Interface light###
+# apt-get install lxde-core
+
+# alternatives ### 
+#apt-get install virtualbox
+
+### NetWorking ####
+#apt-get install nmap
 
 #uninstall
 #apt-get remove --purge gedit links
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-curl https://raw.githubusercontent.com/rjotac/linuxUtilities/master/.vimrc > ~/.vimrc
+#git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#curl https://raw.githubusercontent.com/rjotac/linuxUtilities/master/.vimrc > ~/.vimrc
 
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl https://raw.githubusercontent.com/rjotac/linuxUtilities/master/.vimrc > ~/.vimrc
 vim -c 'PluginInstall' -c 'qa!'
+
+#vim -c 'PluginInstall' -c 'qa!'
 
