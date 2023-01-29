574e36bc921dce6ce702edfd4524bf5bf2e07d66
@Julnarot <rauljhonatan.tola@gmail.com>
2023-01-28 22:10:42 -0500

    setting path for pipenv

--- programs-install/basicsProgramsWSL.sh
+++ programs-install/basicsProgramsWSL.sh
@@ -1,3 +1,5 @@
 apt-get install libtool libtool-bin autoconf automake cmake g++ pkg-config unzip gettext
 
-
+## Execute  these sentese after install pipenv
+# PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"
+# PATH="$PATH:$PYTHON_BIN_PATH"
